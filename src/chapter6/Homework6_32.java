cdd98562d08aa978af609de8831cc20f8dcab894
bobby <bobby@DESKTOP-ADVU6A1.attlocal.net>
2020-09-27 20:02:25 -0600

    Just added the program requirements

---
+++ src/chapter6/Homework6_32.java
@@ -0,0 +1,11 @@
+package chapter6;
+
+public class Homework6_32 {
+
+	public static void main(String[] args) {
+		/*6.32 (Game: chance of winning at craps)
+		 *Revise Exercise 6.30 to run it 10,000 times and display the number of winning games.*/
+
+	}
+
+}
