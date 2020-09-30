83287706e99f3860e03bbf136018accde8fac5e1
Charlesccharris <harriscc@student.swosu.edu>
2020-09-29 22:01:43 -0500

    Got rid of millions multiplier because it took forever, and eventually just stopped

--- src/chapter6/Homework6_32.java
+++ src/chapter6/Homework6_32.java
@@ -1,15 +1,34 @@
+import java.util.Scanner;
+
 public class Homework6_32 {
 
 
 	public static void main(String[] args) {
 		/*6.32 (Game: chance of winning at craps)
 		 *Revise Exercise 6.30 to run it 10,000 times and display the number of winning games.*/
-          	int currentRound = 1;
+          	Scanner input = new Scanner(System.in);
+
+		int currentRound = 1;
 		int wins = 0;
 		int losses = 0;
 		int determine = 0;
+		int totalRounds;
+		char multiplier = 'n';
 
-		for(currentRound = 1; currentRound <= 10_000; currentRound++){
+		System.out.print("Enter a number of rolls to be multiplied (1-999): ");
+		totalRounds = input.nextInt();
+
+		if(totalRounds > 999 || totalRounds < 1){
+			System.out.println("I told you to put in a number in the range of 1-999, it's not that hard to follow instructions.");
+			System.exit(1);
+		}
+		System.out.println("\nNow enter a multiplier, (N)one, (T)housands: ");
+		multiplier = input.next().charAt(0);
+		multiplier = Character.toUpperCase(multiplier);
+
+		totalRounds = numberOfRounds(totalRounds, multiplier);
+
+		for(currentRound = 1; currentRound <= totalRounds; currentRound++){
 			int diceOne = diceRolls();
         	        int diceTwo = diceRolls();
 
@@ -49,6 +68,19 @@ public class Homework6_32 {
 		return roll;
 	}
 
+	public static int numberOfRounds(int totalRounds, char multiplier){
+
+		if(multiplier == 'N')
+			totalRounds *= 1;
+		else if(multiplier == 'T')
+			totalRounds *= 1_000;
+		else{
+			System.out.println("That's not a valid input");
+			totalRounds *= 0;
+			System.exit(1);
+		}
+		return totalRounds;
+	}
 	public static int rollAgain(int total){
 
                 int rollOne = diceRolls();
